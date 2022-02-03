2643e0b7792ec0438745d566f60b658d36e21f5b
TheaKersh <83984604+TheaKersh@users.noreply.github.com>
2022-02-03 10:49:37 -0800

    Update Node.go

--- Node.go
+++ Node.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 	//"encoding/xml"
-	"bufio"
-	"strings"
+	//"bufio"
+	//"strings"
 )
 
 type Index struct {
@@ -15,29 +15,25 @@ type Index struct {
   year int
 }
 
-func getRequest(w http.ResponseWriter, r *http.Request) {
-  resp, err := http.Get("https://data.un.org/ws/rest/data/DF_UNData_UNFCC")
+func check(err error) {
   if err != nil {
     log.Fatal(err)
   }
+}
+
+func getRequest(w http.ResponseWriter, r *http.Request) {
+  client := &http.Client{}
+  req, err := http.NewRequest("GET", "https://data.un.org/ws/rest/data/DF_UNData_UNFCC", nil)
+  check(err)
+  req.Header.Set("Accept", "text/json")
+  resp, err := client.Do(req)
+  check(err)  
 
   buf := new(bytes.Buffer)
   buf.ReadFrom(resp.Body)
-  out := string(buf.Bytes())
-  out = string(out[len("http://esa.un.org/unpd/wpp/index.htm"):])
-  fmt.Print(out)
-
-  
   defer resp.Body.Close()
   fmt.Fprintf(w, "%s", string(buf.Bytes()))
-  reader := bufio.NewReader(strings.NewReader(out))
-  for true {
-    str, err := reader.ReadString('\n')
-    if err != nil {
-      panic(err)
-    }
-    str 
-  }
+  //reader := bufio.NewReader(strings.NewReader(out))
 }
 
 func main(){
