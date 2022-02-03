c86c6539a40cf16a58e4d43f07cf7336abfb48f6
TheaKersh <83984604+TheaKersh@users.noreply.github.com>
2022-02-03 13:34:11 -0800

    Update Node.go

--- Node.go
+++ Node.go
@@ -21,9 +21,20 @@ func check(err error) {
   }
 }
 
+func initNameQueryMap() map[string]string { 
+  var m map[string]string = make(map[string]string)
+  m["Greenhouse Gases"] = "DF_UNData_UNFCC"
+  m["Carbon"] = m["Greenhouse Gases"] + "/A.EN_ATM_PFCE.AUs+DNK.Gg_CO2"
+  return m
+  
+}
+
 func getRequest(w http.ResponseWriter, r *http.Request) {
   client := &http.Client{}
-  req, err := http.NewRequest("GET", "https://data.un.org/ws/rest/data/DF_UNData_UNFCC", nil)
+  //starttime := "1995"
+  //endtime := "2001"
+  m := initNameQueryMap()
+  req, err := http.NewRequest("GET", "https://data.un.org/ws/rest/data/" + m["Carbon"], nil)
   check(err)
   req.Header.Set("Accept", "text/json")
   resp, err := client.Do(req)
@@ -33,10 +44,11 @@ func getRequest(w http.ResponseWriter, r *http.Request) {
   buf.ReadFrom(resp.Body)
   defer resp.Body.Close()
   fmt.Fprintf(w, "%s", string(buf.Bytes()))
-  //reader := bufio.NewReader(strings.NewReader(out))
 }
 
 func main(){
   http.HandleFunc("/view/", getRequest)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+
